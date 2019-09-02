3cf9ceebc4b962db4606ad9f69abedaf8e298d58
Pavel <marchukpavelp@gmail.com>
2019-09-03 00:14:02 +0300

    Yet another release

--- shopping-web/src/components/Header.vue
+++ shopping-web/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <ul class="navbar-nav mr-auto">
-            <li v-for="item in items" class="nav-item" v-bind:class="{ active: item.isActive }">
+            <li v-for="item in items" :key="item.name" class="nav-item" v-bind:class="{ active: item.isActive }">
                 <a class="nav-link" href="#" @click="activateItem(item)">{{ item.name }}</a>
             </li>
         </ul>
